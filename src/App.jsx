d5163c1ce96e22d6abe088cf73e16d2d08f536a6
Netpoc <ayenotopeter@hotmail.com>
2023-07-16 23:21:07 +0000

    move

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,26 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import SideBar from './sideBar';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 function ContainerFluidBreakpointExample() {
   return (
     <Container fluid="md">
       <Row className="mt-5">
-        <Col md={2}>
+        <Col md={8}>
+          <Row>
+          <Col md={3}>
+          <Form.Label>Games</Form.Label>
         <Form.Select aria-label="Default select example">
-      <option>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
-    </Col>
-    <Col md={2}>
-    <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <option>Select Game Type</option>
+          <option value="1">Type A</option>
+          <option value="2">Type B</option>
+          <option value="3">Type C</option>
+        </Form.Select>
+        </Col>
+        <Col md={2}>
+        <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
                 <Form.Label>Average Stake</Form.Label>
                 <Form.Control size="sm" type="number"/>
             </Form.Group>
@@ -29,13 +33,26 @@ function ContainerFluidBreakpointExample() {
                 <Form.Control size="sm" type='number'/>
             </Form.Group>
             </Form>
-    </Col>
-    <Col>
-    </Col>
-    <Col md={3}>
-    <SideBar />
-    </Col>
-    </Row>     
+        </Col>
+        <Col>
+        
+        </Col>
+          </Row>
+          <Row>
+            <Col>
+            <p aria-hidden="true">
+        <Placeholder md={8} />
+      </p>
+
+      <Placeholder.Button md={4} aria-hidden="true" />
+            </Col>
+          </Row>
+        </Col>
+        <Col></Col>
+        <Col md={3}>
+        <SideBar />
+        </Col>
+      </Row>     
         
     </Container>
   );
