8a1e92678b5393741f35047b2cde749bd83d50af
Taiwo Akinde <taiwo.akinde@outlook.com>
2023-07-18 17:23:01 +0100

    enhanced the UI

--- src/sideBar.jsx
+++ src/sideBar.jsx
@@ -1,62 +1,94 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import { Col } from 'react-bootstrap';
+import {Button, Card, Row, Container, Col} from "react-bootstrap";
+import { AiFillWarning } from "react-icons/ai";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { useState } from "react";
 
-function SideBar() {
-    return (
-        <Container fluid="md">
-        <Row>
-            <Col>
-            <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="https://res.cloudinary.com/netpoc-inc/image/upload/v1689637069/12_ogqa3m.jpg" />
-    <Card.Body>
-        <Card.Title>Simulation Result</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the content.
-            </Card.Text>
-            <Row>
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function SideBar({ imageList }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleImageClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <Container fluid="md">
+      <Row>
+        <Col>
+          <Card style={{ width: "18rem", className: "my-auto" }}>
+            <Card.Img
+              variant="top"
+              //   src="https://res.cloudinary.com/netpoc-inc/image/upload/v1689637069/12_ogqa3m.jpg"
+              src={imageList[0]}
+              className={`custom-card-img ${isExpanded ? "expanded" : ""}`}
+              onClick={handleImageClick}
+            />
+            <Card.Body>
+              <Card.Title className="d-flex justify-content-center align-items-center">
+                Simulation Result
+              </Card.Title>
+              <div className="d-flex justify-content-center align-items-center">
+                {" "}
+                <AiFillWarning size={35} />
+              </div>
+
+              <Card.Text className="d-flex">
+                Carefully review this simulated result and be sure it meets
+                business expectations
+              </Card.Text>
+              <Row>
                 <Col>
-                <Button variant="primary">Accept</Button>
+                  <Button variant="outline-primary">Accept</Button>
                 </Col>
                 <Col></Col>
                 <Col>
-                <Button variant="danger">Reset</Button>
+                  <Button variant="outline-danger">Reset</Button>
                 </Col>
-            </Row>
-            
-            
-    </Card.Body>
-</Card>
-            </Col>
-        </Row>
-        <Row className="mt-3">
-            <Col>
-            <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="https://res.cloudinary.com/netpoc-inc/image/upload/v1689637069/12_ogqa3m.jpg" />
-    <Card.Body>
-        <Card.Title>All Simulations</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the content.
-            </Card.Text>
-            <Row>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <Card style={{ width: "18rem", className: "my-auto"  }}>
+            <Card.Img
+              variant="top"
+              //   src="https://res.cloudinary.com/netpoc-inc/image/upload/v1689637069/12_ogqa3m.jpg"
+              src={imageList[1]}
+              className={`custom-card-img ${isExpanded ? "expanded" : ""}`}
+              onClick={handleImageClick}
+            />
+            <Card.Body>
+              <Card.Title className="d-flex justify-content-center align-items-center">
+                All Simulations
+              </Card.Title>
+              <div className="d-flex justify-content-center align-items-center">
+                {" "}
+                <AiFillWarning size={35} />
+              </div>
+
+              <Card.Text>
+                Carefully review this simulated result and be sure it meets
+                business expectations
+              </Card.Text>
+              <Row>
                 <Col>
-                <Button variant="primary">Accept</Button>
+                  <Button variant="outline-primary">Accept</Button>
                 </Col>
                 <Col></Col>
                 <Col>
-                <Button variant="danger">Reset</Button>
+                  <Button variant="outline-danger">Reset</Button>
                 </Col>
-            </Row>
-    </Card.Body>
-</Card>
-            </Col>
-        </Row>        
-        </Container>
-    )
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SideBar;
